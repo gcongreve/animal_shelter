ff1856f19ee74cf7753ccc3c09e9f7d18095776f
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-09 14:47:27 +0000

    added new erb file and method to animals

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -7,11 +7,18 @@ get '/animals/?' do
   erb ( :'animals/index' )
 end
 
-#show individual
-get '/animals/:id/?' do
-  @animal = Animal.find_by_id(params[:id])
-  erb ( :'animals/show' )
+# new
+get '/animals/new/?' do
+  erb(:'animals/new')
 end
+
+# create
+post '/animals/?' do
+  @animal = Animal.new(params)
+  @animal.save
+  erb(:'animals/create')
+end
+
 
 # edit
 get '/animals/:id/edit/?' do
@@ -19,9 +26,29 @@ get '/animals/:id/edit/?' do
   erb( :'animals/edit')
 end
 
-# update
+#update
 post '/animals/:id/?' do
   @animal = Animal.new(params)
   @animal.update
-  redirect to '/animals/' + params[:id]
+  erb ( :'animals/update')
 end
+
+#show individual
+get '/animals/:id/?' do
+  @animal = Animal.find_by_id(params[:id])
+  erb ( :'animals/show' )
+end
+
+
+
+  # <br>
+  # <label for="adoptable">Date Admitted: </label>
+  # <input id="adoptable" type="text" name="adoptable" value="<%= @animal.adoptable %>">
+  #
+  # <br>
+  # <label for="trained">Date Admitted: </label>
+  # <input id="trained" type="text" name="trained" value="<%= @animal.trained %>">
+  #
+  # <br>
+  # <label for="adopted">Date Admitted: </label>
+  # <input id="adopted" type="text" name="adopted" value="<%= @animal.adopted %>">
