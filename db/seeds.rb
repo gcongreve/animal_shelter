d2f76c369cec91967fd6f94fac57b00ccc82501c
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-09 12:01:23 +0000

    added seeds and created index view for animal

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,22 @@
 require_relative('../models/animal')
 require('pry-byebug')
+
+Animal.delete_all
+
+animal_a = Animal.new({
+  "name" => "Alf",
+  "type" => "Dog",
+  "date_admitted" => "10/10/10",
+  "adoptable" => true,
+  "trained" => true,
+  "adopted" => false
+  })
+
+animal_b = Animal.new({
+  "name" => "Bert",
+  "type" => "Cat",
+  "date_admitted" => "11/11/11",
+  "adoptable" => true,
+  "trained" => true,
+  "adopted" => false
+  })
--- models/animal.rb
+++ models/animal.rb
@@ -33,7 +33,7 @@ class Animal
 
   def delete
     sql = "DELETE FROM animals
-            WHERE id = $1"
+          WHERE id = $1"
     values = @id
     SqlRunner.run(sql, values)
 
