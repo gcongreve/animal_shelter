a43c0a487972f1e6c529ac1094eea2371165803d
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-09 14:08:49 +0000

    Added edit.erb to animals

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -12,3 +12,16 @@ get '/animals/:id/?' do
   @animal = Animal.find_by_id(params[:id])
   erb ( :'animals/show' )
 end
+
+# edit
+get '/animals/:id/edit/?' do
+  @animal = Animal.find_by_id(params[:id])
+  erb( :'animals/edit')
+end
+
+# update
+post '/animals/:id/?' do
+  @animal = Animal.new(params)
+  @animal.update
+  redirect to '/animals/' + params[:id]
+end
