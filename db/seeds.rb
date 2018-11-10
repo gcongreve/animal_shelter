16fcc6133c2b3b8d6fe8a244d408c7cfb1fbf14b
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-10 10:54:24 +0000

    added .save method to Adoption class, repopulated seeds file. Adoptions can be saved to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require_relative('../models/animal.rb')
 require_relative('../models/customer.rb')
+require_relative('../models/adoption.rb')
 require('pry-byebug')
 
 Animal.delete_all
@@ -22,6 +23,9 @@ animal_b = Animal.new({
   "trained" => true,
   })
 
+  animal_a.save
+  animal_b.save
+
   customer_a = Customer.new({
     "first_name" => "Dave",
     "last_name" => "Human",
@@ -34,7 +38,18 @@ animal_b = Animal.new({
     "number_of_adoptions" => 1
     })
 
-  animal_a.save
-  animal_b.save
   customer_a.save
   customer_b.save
+
+  adoption_a = Adoption.new({
+    "animal_id" => animal_a.id,
+    "customer_id" => customer_a.id,
+    })
+
+  adoption_b = Adoption.new({
+    "animal_id" => animal_b.id,
+    "customer_id" => customer_b.id,
+    })
+
+  adoption_a.save
+  adoption_b.save
