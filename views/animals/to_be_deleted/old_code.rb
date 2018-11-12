2853e2be837310e493d2c8b6199e079fbcf026e5
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-12 18:01:47 +0000

    edited cusotmers show view

---
+++ views/animals/to_be_deleted/old_code.rb
@@ -0,0 +1,5 @@
+<p>First Name: <%=@customer.first_name %></p>
+<p>Last Name: <%=@customer.last_name %></p>
+<p>Species Preference : <%= @customer.species_preference %></p>
+<p>Type Preference : <%= @customer.type_preference %></p>
+<p>Number of Adoptions: <%=@customer.number_of_adoptions %></p>
